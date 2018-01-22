2729342cbb14ce280df40abbdb6c71e8467e8271
shreesakthi <35295083+shreesakthi@users.noreply.github.com>
2018-01-22 16:53:21 +0530

    VOWEL.C
    to check whether a character is vowel or constant

---
+++ VOWEL.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int lower case vowel,upper case vowel;
+char c;
+printf("enter an aiphabet:");
+scanf("%c",&c);
+printf("%c is a vowel",c);
+else
+printf("%c is a constant",c);
+return 0;
+}
